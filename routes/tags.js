4b8575bb0ae19804ba6e228beab6179fbfdfed50
anerold <muliku@seznam.cz>
2020-02-11 18:17:31 +0100

    still working on GET tags...

--- routes/tags.js
+++ routes/tags.js
@@ -5,21 +5,31 @@ let Articles = require('../models/articles');//import model
 let Tags = require('../models/tags');
 
 router.get('/', function (req, res) {
-    Tags.find({}, {_id: 0, __v: 0}, function (err, docs) {
-        console.log(docs);
+    Tags.find({}, { _id: 0, __v: 0 }, function (err, docs) {
         if (err) {
             res.status(500).send('Internal Server Error.');
         } else {
+            var allArticleIDs = [];
+            for (var i = 0; i < docs.length; i++) {
+                for(var j = 0; j<docs[i].articles.length;j++){
+                    if(!(allArticleIDs.indexOf(String(docs[i].articles[j])) > -1)){ //if not already in array, push it
+                        allArticleIDs.push(String(docs[i].articles[j]));
+                    }
+                }
+                
+            }
 
             Articles.find({
-                '_id': {$in: docs.articles}
+                '_id': { $in: allArticleIDs }
             }, function (err, articles) {
+                console.log(articles);
+                console.log("///////////////////////////////////")
+                console.log(docs);
                 if (err) {
-                    res.status(500).send(JSON.stringify({message: "Internal server error"}));
+                    res.status(500).send(JSON.stringify({ message: "Internal server error" }));
                 } else {
-                    console.log(articles);
-                    for (var i = 0; i < articles.length; i++) {
-                        docs.articles[i] = articles[i].title; //rewrite ids of tags in array with actual names of the tags 
+                    for (var i = 0; i < docs.length; i++) {
+                        docs[i].articles[i] = articles[i].title; //rewrite ids of tags in array with actual names of the tags 
                     }
                     res.status(200).send(docs);
                 }
