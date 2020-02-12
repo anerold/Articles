50d59aa65b238ab0e45febab31734609905e58e9
Adam Neřold <anerold@spel.cz>
2020-02-12 11:15:01 +0100

    Finished GET tags

--- routes/tags.js
+++ routes/tags.js
@@ -5,41 +5,37 @@ let Articles = require('../models/articles');//import model
 let Tags = require('../models/tags');
 
 router.get('/', function (req, res) {
-    Tags.find({}, { _id: 0, __v: 0 }, function (err, docs) {
+    //first create pairs {articleID1:articleTitle1, articleID2: articleTitle2, ...}
+    Articles.find({}, function (err, articles) {
         if (err) {
-            res.status(500).send('Internal Server Error.');
+            res.status(500).send(JSON.stringify({message: "Internal server error"}));
         } else {
-            var allArticleIDs = [];
-            for (var i = 0; i < docs.length; i++) {
-                for(var j = 0; j<docs[i].articles.length;j++){
-                    if(!(allArticleIDs.indexOf(String(docs[i].articles[j])) > -1)){ //if not already in array, push it
-                        allArticleIDs.push(String(docs[i].articles[j]));
-                    }
+            articleTitles = {};
+            for (var i = 0; i < articles.length; i++) {
+                if (!articleTitles.hasOwnProperty(articles[i]._id)) {
+                    articleTitles[articles[i]._id] = articles[i].title;
                 }
-                
             }
-
-            Articles.find({
-                '_id': { $in: allArticleIDs }
-            }, function (err, articles) {
-                console.log(articles);
-                console.log("///////////////////////////////////")
-                console.log(docs);
+            //then list all tags, rewrite their article's ids with its names and send to client:
+            Tags.find({}, {_id: 0, __v: 0}, function (err, tags) {  //ommit _id and __v
                 if (err) {
-                    res.status(500).send(JSON.stringify({ message: "Internal server error" }));
+                    res.status(500).send('Internal Server Error.');
                 } else {
-                    for (var i = 0; i < docs.length; i++) {
-                        docs[i].articles[i] = articles[i].title; //rewrite ids of tags in array with actual names of the tags 
+
+                    for (var i = 0; i < tags.length; i++) { // iterate trough all tags
+                        for (var j = 0; j < tags[i].articles.length; j++) { //iterate trough tag's articles
+                            tags[i].articles[j] = articleTitles[tags[i].articles[j]]; //change article's id to article's name (because we don't want to send the client DB's IDs)
+                        }
                     }
-                    res.status(200).send(docs);
+                    res.status(200).send(tags);
+
                 }
             });
 
-
-
-
         }
     });
+
+
 });
 
 
